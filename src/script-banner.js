7e0d53a2aaa7cdd0b1b9e0fa2f072bebab914078
dev-carl-robert <dev.carl.robert@gmail.com>
2024-03-22 23:30:00 -0300

    produtos

--- src/script-banner.js
+++ src/script-banner.js
@@ -1,15 +1,53 @@
 let count = 1;
 document.getElementById("radio1").checked = true;
 
-setInterval( function(){
+setInterval(function () {
     nextImage()
-}, 5000)
+}, 6000)
 
-function nextImage(){
+function nextImage() {
     count++;
-    if (count>4){
+    if (count > 4) {
         count = 1;
     }
 
-    document.getElementById("radio"+count).checked = true;
+    document.getElementById("radio" + count).checked = true;
 }
+const progressContent = document.querySelector(".autoplay-progress span");
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  slidesPerView: 3,
+  centeredSlides: true,
+  fade: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  keyboard: true,
+  on: {
+    autoplayTimeLeft(s, time, progress) {
+      progressContent.textContent = `${Math.ceil(time / 1200)}s`;
+    },
+
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 1,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
+});
