f5273696ad69d910d77234bdc68536b62a766c7b
dev-carl-robert <dev.carl.robert@gmail.com>
2024-03-22 23:32:30 -0300

    produto

--- src/script-banner.js
+++ src/script-banner.js
@@ -13,41 +13,9 @@ function nextImage() {
 
     document.getElementById("radio" + count).checked = true;
 }
+
 const progressContent = document.querySelector(".autoplay-progress span");
 var swiper = new Swiper(".mySwiper", {
   spaceBetween: 30,
   slidesPerView: 3,
-  centeredSlides: true,
-  fade: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev"
-  },
-  keyboard: true,
-  on: {
-    autoplayTimeLeft(s, time, progress) {
-      progressContent.textContent = `${Math.ceil(time / 1200)}s`;
-    },
-
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    520: {
-      slidesPerView: 1,
-    },
-    950: {
-      slidesPerView: 3,
-    },
-  },
-});
+  centeredSlides: true
